36413a355655f4ca75a2510a84cdc6c3e56df6c6
Prasanan Mestha <prasanna.mestha@practiskills.com>
2020-01-08 20:35:01 +0530

    Added expertise section

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import About from './components/About'
+import Expertise from './components/Expertise'
 
 const App = () => {
     return (
         <>
             <About />
+            <Expertise />
         </>
     )
 }
