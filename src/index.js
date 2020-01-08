0cd7362b72730593f9128b1f7b4ed6d98077ef45
Prasanan Mestha <prasanna.mestha@practiskills.com>
2020-01-08 20:14:33 +0530

    Initial structure of the project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <p>Hello!</p>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
